418d3ac61ede4497e24fd89fc6a110f5b5dbaa73
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-07-16 15:59:27 +0300

    feat: add images

--- Workshop-Origami/src/components/header/index.js
+++ Workshop-Origami/src/components/header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './index.module.css';
 import Link from '../link';
+import image from '../../images/white-origami-bird.png';
 
 function Header() {
     return (
         <header className={styles.navigation}>
             <ul>
+            <img src={image}/>
                 <Link href='#' title='Going to 1' type='header'/>
                 <Link href='#' title='Going to 2' type='header'/>
                 <Link href='#' title='Going to 3' type='header'/>

d1b431e24a2b73b994fc497bbd9134578135d2b6
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-08-01 15:13:56 +0300

    feat: refactoring

--- Workshop-Origami/src/Authorization.js
+++ Workshop-Origami/src/Authorization.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import UserContext from './Context';
 
 function getCookie(name) {
@@ -6,72 +6,126 @@ function getCookie(name) {
     return cookieValue ? cookieValue[2] : null;
 }
 
-class Authorization extends Component {
-    constructor(props) {
-        super(props)
+function Authorization(props) {
+    const [user, setUser] = useState(null);
+    const [loggedIn, setLoggedIn] = useState(false);
 
-        this.state = {
-            loggedIn: false,
-            user: null
-        }
-    }
-    componentDidMount = async () => {
+    useEffect(() => {
         const token = getCookie('token');
-        console.log(token);
 
         if (!token) {
-            this.logout();
+            logout();
             return
         }
 
-        const promise = await fetch('http://localhost:4000/api/user/verify', {
-            method: 'POST',
-            body: JSON.stringify({ token }),
+        fetch('http://localhost:4000/api/user/verify', {
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': token
             }
         })
-        try {
-            const res = await promise.json();
+            .then(promise => {
+                return promise.json();
+            })
+            .then(res => {
+                res.status
+                    ? login({
+                        username: res.user.username,
+                        id: res.user._id
+                    })
+                    : logout()
+            })
+            .catch(err => console.log(err))
+    }, []);
 
-            res.status
-                ? this.login({
-                    username: res.user.username,
-                    id: res.user._id
-                })
-                : this.logout()
-        }
-        catch (error) {
-            console.log(error);
-        }
-
+    const login = (user) => {
+        setUser(user);
+        setLoggedIn(true);
     }
-    login = (user) => {
-        this.setState({
-            loggedIn: true,
-            user
-        })
-    }
-    logout = () => {
+    const logout = () => {
         document.cookie = 'token' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        this.setState({
-            loggedIn: false,
-            user: null
-        })
+        setLoggedIn(false);
+        setUser(null);
     }
-    render() {
-        const { loggedIn, user } = this.state;
-        return (
-            <UserContext.Provider value={{
-                loggedIn,
-                user,
-                login: this.login,
-                logout: this.logout
-            }}>
-                {this.props.children}
-            </UserContext.Provider>
-        );
-    }
+    return (
+        <UserContext.Provider value={{
+            loggedIn,
+            user,
+            login,
+            logout
+        }}>
+            {props.children}
+        </UserContext.Provider>
+    )
 }
+
+
+// class Authorization extends Component {
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+//             loggedIn: false,
+//             user: null
+//         }
+//     }
+//     componentDidMount = async () => {
+//         const token = getCookie('token');
+//         console.log(token);
+
+//         if (!token) {
+//             this.logout();
+//             return
+//         }
+
+//         const promise = await fetch('http://localhost:4000/api/user/verify', {
+//             method: 'POST',
+//             body: JSON.stringify({ token }),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//         try {
+//             const res = await promise.json();
+
+//             res.status
+//                 ? this.login({
+//                     username: res.user.username,
+//                     id: res.user._id
+//                 })
+//                 : this.logout()
+//         }
+//         catch (error) {
+//             console.log(error);
+//         }
+//     }
+//     login = (user) => {
+//         this.setState({
+//             loggedIn: true,
+//             user
+//         })
+//     }
+//     logout = () => {
+//         document.cookie = 'token' + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+//         this.setState({
+//             loggedIn: false,
+//             user: null
+//         })
+//     }
+//     render() {
+//         const { loggedIn, user } = this.state;
+//         return (
+//             <UserContext.Provider value={{
+//                 loggedIn,
+//                 user,
+//                 login: this.login,
+//                 logout: this.logout
+//             }}>
+//                 {this.props.children}
+//             </UserContext.Provider>
+//         );
+//     }
+// }
 
 export default Authorization;
--- Workshop-Origami/src/pages/login/index.js
+++ Workshop-Origami/src/pages/login/index.js
@@ -1,38 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useContext } from 'react';
 import styles from './index.module.css';
 import Wrapper from '../../components/page-wrapper';
 import Button from '../../components/button';
 import Input from '../../components/input';
 import UserContext from '../../Context';
 
-class Login extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-    static contextType = UserContext;
+function LoginPage(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const context = useContext(UserContext);
 
-    onChange = (event, type) => {
-        const newState = {};
-        newState[type] = event.target.value;
-        this.setState(newState);
-    }
-    onSubmit = async (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        const {
-            username,
-            password,
-        } = this.state;
 
         if (!username || !password) {
             alert('Invalid username or password');
             return;
         }
-        const promise = await fetch('http://localhost:4000/api/user/login', {
+        fetch('http://localhost:4000/api/user/login', {
             method: 'POST',
             body: JSON.stringify({
                 username,
@@ -42,52 +28,136 @@ class Login extends Component {
                 'Content-Type': 'application/json'
             }
         })
-        try {
-            if (promise.ok) {
-                const token = promise.headers.get('Authorization');
-                document.cookie = `token=${token}`;
-
-                const data = await promise.json();
+            .then(promise => {
+                if (promise.ok) {
+                    const token = promise.headers.get('Authorization');
+                    document.cookie = `token=${token}`;
+                    return promise.json();
+                }
+            })
+            .then(data => {
                 const user = {
                     username: data.username,
                     id: data._id
                 }
-                this.context.login(user);
-
-                this.props.history.push('/');
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    }   
-    render() {
-        const { username, password } = this.state;
-        return (
-            <Wrapper>
-                <div className={styles.container}>
-                    <h1 className={styles.title}>Login Page</h1>
-                    <form onSubmit={this.onSubmit}>
-                        <Input
-                            value={username}
-                            onChange={(e) => this.onChange(e, 'username')}
-                            label='Username'
-                            id='username'
-                        />
-                        <Input
-                            value={password}
-                            onChange={(e) => this.onChange(e, 'password')}
-                            label='Password'
-                            id='password'
-                        />
-                        <div>
-                            <Button type='submit' title='Login' />
-                        </div>
-                    </form>
-
-                </div>
-            </Wrapper>
-        );
+                context.login(user);
+                props.history.push('/');
+            })
+            .catch(error => console.log(error))
     }
+
+    return (
+        <Wrapper>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Login Page</h1>
+                <form onSubmit={onSubmit}>
+                    <Input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        label='Username'
+                        id='username'
+                    />
+                    <Input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        label='Password'
+                        id='password'
+                    />
+                    <div>
+                        <Button type='submit' title='Login' />
+                    </div>
+                </form>
+
+            </div>
+        </Wrapper>
+    )
 }
 
-export default Login;
+
+
+
+// class Login extends Component {
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+//             username: '',
+//             password: ''
+//         }
+//     }
+//     static contextType = UserContext;
+
+//     onChange = (event, type) => {
+//         const newState = {};
+//         newState[type] = event.target.value;
+//         this.setState(newState);
+//     }
+//     onSubmit = (e) => {
+//         e.preventDefault();
+//         const {
+//             username,
+//             password,
+//         } = this.state;
+
+//         if (!username || !password) {
+//             alert('Invalid username or password');
+//             return;
+//         }
+//         fetch('http://localhost:4000/api/user/login', {
+//             method: 'POST',
+//             body: JSON.stringify({
+//                 username,
+//                 password
+//             }),
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         })
+//             .then(promise => {
+//                 if (promise.ok) {
+//                     const token = promise.headers.get('Authorization');
+//                     document.cookie = `token=${token}`;
+//                     return promise.json();
+//                 }
+//             })
+//             .then(data => {
+//                 const user = {
+//                     username: data.username,
+//                     id: data._id
+//                 }
+//                 this.context.login(user);
+//                 this.props.history.push('/');
+//             })
+//             .catch(error => console.log(error))
+//     }
+//     render() {
+//         const { username, password } = this.state;
+//         return (
+//             <Wrapper>
+//                 <div className={styles.container}>
+//                     <h1 className={styles.title}>Login Page</h1>
+//                     <form onSubmit={this.onSubmit}>
+//                         <Input
+//                             value={username}
+//                             onChange={(e) => this.onChange(e, 'username')}
+//                             label='Username'
+//                             id='username'
+//                         />
+//                         <Input
+//                             value={password}
+//                             onChange={(e) => this.onChange(e, 'password')}
+//                             label='Password'
+//                             id='password'
+//                         />
+//                         <div>
+//                             <Button type='submit' title='Login' />
+//                         </div>
+//                     </form>
+
+//                 </div>
+//             </Wrapper>
+//         );
+//     }
+// }
+
+export default LoginPage;
